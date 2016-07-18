bbfde731443d3c6536270e194a72cfff96750cbd
supdood <efetsko@umail.iu.edu>
2016-07-18 00:16:44 -0400

    Added the ability to convert champion id's to names without an api call.

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,32 +1,30 @@
-myApp.controller('HomeController', ['$scope', '$http', 'Summoner', function($scope, $http, Summoner){
+myApp.controller('HomeController', ['$scope', '$http', 'Summoner', function ($scope, $http, Summoner) {
   var key = '?api_key=RGAPI-7A978D8D-2FF6-44D1-89CC-6E73ADE01034';
   var pvpnet = 'https://na.api.pvp.net';
   var playerID;
-  
-  $.getJSON('js/json/champions.json', function(champData){
-    //$scope.champs = getJsonArray(champData.data);
-  }); 
-  
-  $scope.getInfo = function(name){
-    Summoner.basicInfo(name).then(function(response){
+
+  $scope.getInfo = function (name) {
+    Summoner.basicInfo(name).then(function (response) {
       $scope.basicInfo = response;
     });
   };
-  
-  $scope.getRankedStats = function(id, name) {
-    Summoner.getRankedStats(id, name).then(function(response){
-      $scope.rankedStats = response;
+
+  $scope.getRankedStats = function () {
+    $scope.$watch("basicInfo", function () {
+
+      console.log($scope.basicInfo);
+      Summoner.getRankedStats($scope.basicInfo.id).then(function (response) {
+        $scope.rankedStats = response;
+      });
     });
   };
-  
-  $scope.championByID = function(id) {
-    Summoner.championByID(id).then(function(response){
-      $scope.champs = response;
-    });
+
+  $scope.championByID = function (id) {
+    $scope.champs = Summoner.championByID(id);
   };
-  
+
   $scope.getInfo("supdood");
-  $scope.getRankedStats(99, "supdood");
-  $scope.championByID("99");
-  
+  $scope.getRankedStats("supdood");
+  $scope.championByID(99);
+
 }]);
